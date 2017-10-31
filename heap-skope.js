70dc865e5864c31fe3e7ff8b97d19efb34285e64
Greg Lawrence <gregaudio@gmail.com>
2017-10-31 14:17:45 -0500

    fixed last if statement to check for a partially full container, or not to check if mine has more gems than will fit in available containers

--- heap-skope.js
+++ heap-skope.js
@@ -16,7 +16,7 @@ const gemHeapSkope = function () { // No parameter needed
     */
     const GemMine = {
         "Onyx": {
-            "kilograms": 40053
+            "kilograms": 453
         },
         "Amethyst": {
             "kilograms": 453
@@ -75,8 +75,6 @@ const gemHeapSkope = function () { // No parameter needed
             }
         }
     }
-
-
 }
 
 /*
@@ -92,99 +90,84 @@ const SkopeManager = gemHeapSkope()
 const gemsPerOrder = 5;
 const maximumContainers = 30;
 
-// setup a try/catch to catch errors from not having enough containers, in case the amount of gems in gemMine changes. You can test this try/catch by adding changing a gem amount in the gemMine to something large like 40,000
-try {
-    
-    // create an order of gems
-    const gemSequence = ["Onyx", "Amethyst", "Bloodstone", "Emerald"]
-    
-    // create a staging array for processed gems to be held in before they are placed into containers
-    const processedOrders = [];
-    
-    // Loop over array gemSequence and run function for each gem. When the amount of gems in the gemOrder is 0, move to the next gem in the sequence
-    gemSequence.forEach(function (gem) {
-        
-        do {
-            // loop to fill this container until until the gemOrder total is 0, meaning no gems left of that type.
-            // run an order to process gems
-            gemOrder = SkopeManager.process(gem)
-            processedOrders.push(gemOrder)
-        } while (gemOrder.amount >= gemsPerOrder)
-        
-        
-    })  // END OF forEach Function
-    
-    /*
-    Create a generator for 30 storage containers, which is how many a hëap-skope
-    is equipped with.
-    */
-    const gemContainerGenerator = function* () {
-        let currentContainer = 1
-        
-        while (currentContainer <= maximumContainers) {
-            yield { "id": currentContainer, "type": "Mineral", "orders": [] }
-            currentContainer++
-        }
-    }
-    // create instance of gemContainerGenerator
-    const gemContainerFactory = gemContainerGenerator()
-    
-    // create a new container
-    let currentContainer = gemContainerFactory.next().value;
-    
-    // create currentContainerAmount to hold the amount of total kilograms in the container
-    let currentContainerAmount = 0;
-    
-    // create final array to hold all of the containers. 
-    let heapSkopeContainers = [];
-    
-    console.log("processedOrders: ", processedOrders)
 
-    // put the processed orders into containers
-    processedOrders.forEach( function(order){
-        // console.log("currentContainer", currentContainer)
-        // console.log("order", order)
-        // console.log("order.amount", order.amount)
-        
-        if (currentContainer) {  
-            // check that the currentContainer plus the current order are more than what will fit in one container. If it is, push this full container to the heapSkopeContainers array and then create a new container
-            if (currentContainerAmount + order.amount > 565) {
-                // container is now full, push container to keapSkopeContainers
-                heapSkopeContainers.push(currentContainer)
-                // create a new container
-                currentContainer = gemContainerFactory.next().value;
-                currentContainerAmount = 0;
-                let x= 1;
-            }
+// create an order of gems
+const gemSequence = ["Onyx", "Amethyst", "Bloodstone", "Emerald"]
+
+// create a staging array for processed gems to be held in before they are placed into containers
+const processedOrders = [];
+
+// Loop over array gemSequence and run function for each gem. When the amount of gems in the gemOrder is 0, move to the next gem in the sequence
+gemSequence.forEach(function (gem) {
+    
+    do {
+        // loop to fill this container until until the gemOrder total is 0, meaning no gems left of that type.
+        // run an order to process gems
+        gemOrder = SkopeManager.process(gem)
+        processedOrders.push(gemOrder)
+    } while (gemOrder.amount >= gemsPerOrder)
+    
+    
+})  // END OF forEach Function
+
+/*
+Create a generator for 30 storage containers, which is how many a hëap-skope
+is equipped with.
+*/
+const gemContainerGenerator = function* () {
+    let currentContainer = 1
+    
+    while (currentContainer <= maximumContainers) {
+        yield { "id": currentContainer, "type": "Mineral", "orders": [] }
+        currentContainer++
+    }
+}
+// create instance of gemContainerGenerator
+const gemContainerFactory = gemContainerGenerator()
+
+// create a new container
+let currentContainer = gemContainerFactory.next().value;
+
+// create currentContainerAmount to hold the amount of total kilograms in the container
+let currentContainerAmount = 0;
+
+// create final array to hold all of the containers. 
+let heapSkopeContainers = [];
+
+console.log("processedOrders: ", processedOrders)
+
+// put the processed orders into containers
+processedOrders.forEach( function(order){
+  
+    // check if currentContainer is not undefined, which means we are not out of available containers
+    if (currentContainer) { 
+
+        // push an order into the container
+        currentContainer.orders.push(order);
+
+        // increment the size of the current container to reflect this added order
+        currentContainerAmount += order.amount;
             
-            // push an order into the container
-            currentContainer.orders.push(order);
-
-            // increment the size of the current container to reflect this added order
-            currentContainerAmount += order.amount;
-                
-        }
-
-        
-        // else {
-        //     console.log("You are out of containers to store gem orders. Please ask for more containers. Have a good day!")
-        // }
-       
-          
-    })
-
-    //check if there is a container that has processed gems but wasn't pushed since it's not totally full. 
-    if (currentContainer.orders.length > 0) {
-        // push this last container into the heapSkopeContainers array
-        heapSkopeContainers.push(currentContainer)
+        // check that the currentContainer plus the current order are more than what will fit in one container. If it is, push this full container to the heapSkopeContainers array and then create a new container
+        if (currentContainerAmount + order.amount > 565) {
+            // container is now full, push container to keapSkopeContainers
+            heapSkopeContainers.push(currentContainer)
+            // create a new container
+            currentContainer = gemContainerFactory.next().value;
+            currentContainerAmount = 0;
+        }   
     }
+})
 
-    console.log("heapSkopeContainers: ", heapSkopeContainers)
+// Need to catch a container that isn't totally full, and then push to final heapSkopeContainer array
+//debugger
 
+if (currentContainer && currentContainer.orders.length > 0) {
+    // push this last container into the heapSkopeContainers array
+    heapSkopeContainers.push(currentContainer)
 }
-//catch a TypeError which is thrown when there are more gems in the gem mine than we have containers for. This will output a custom error message to the console. 
-catch (e) {
-    if (e instanceof TypeError) {
-        console.log("You are out of containers to store gem orders. Please ask for more containers. Have a good day!")
-    }
-}
+
+//check if there is a container that has processed gems but wasn't pushed since it's not totally full. 
+
+console.log("heapSkopeContainers: ", heapSkopeContainers)
+
